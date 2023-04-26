3eeb5ad98706fcb1fc2282757f3e3cfc824e41e1
MagdalenaPizarroO <123502477+MagdalenaPizarroO@users.noreply.github.com>
2023-04-26 20:44:43 +0000

    apuntes ok

--- src/app.py
+++ src/app.py
@@ -10,8 +10,19 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
 #from models import Person
+#Clase JWT 25 abril '23
+import datetime # necesito saber el tiempo que dura el token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+#JWTManager = mi aplicación en Flask va a trabajar con JWT
+#create_access_token = crea el token
+#jwt_required = valida si el usuario tiene permiso para entrar al sitio
+#get_jwt_identity = permite traer la información que se encuentra encriptada en el token
+
 
 app = Flask(__name__)
+#Vamos a trabajar con JWT:
+jwt = JWTManager(app)
+
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
@@ -45,6 +56,33 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+#Para validar que el usuario y contraseña existen, debo crear una ruta:
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    user = User.query.filter_by(email=body['email']).first()
+    #print(user) #si el usuario no existe, me retorna None (tipo undefined en JS)
+    if (user is None):
+        return jsonify({"mensaje": "el usuario no existe, regístrate por favor"}), 404
+    else:   
+        if(user.password != body['password']):
+            return "la contraseña está mala"    #en la realidad, se pone el usuario o la contraseña son incorrectos; no hay que indicar dónde está el error
+        else:   #usuario y contraseña ok, ahora creo el token
+            expiration = datetime.timedelta(minutes=1) #tiempo que le doy al token par que sea válido
+            token = create_access_token(identity=user.serialize(), expires_delta=expiration)
+            return jsonify({
+                "mensaje": "bienvenido!",
+                "token": token,
+                "tiempo": expiration.total_seconds(),
+                "data": user.serialize()
+            }), 200
+@app.route('/check', methods=['GET'])   #vamos a chequear si el token es válido
+@jwt_required()       #jwt hace la validación solo
+def check_user(): 
+    return jsonify({
+        "logeado": True
+    })
+#en Postman, se pone GET>Auth>Type Bearer Token; 3 respuestas: token OK, token expired, algún error en el token.
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
